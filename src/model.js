09874441afdf7a6f0e47ef9890be87e873906be0
Alexander <50077233+Sekir0@users.noreply.github.com>
2020-08-23 13:30:28 +0300

    created model on js

---
+++ src/model.js
@@ -0,0 +1,16 @@
+export const model = [
+    {
+        type: 'title', value: 'Test title', options: {}
+    },
+    {
+        type: 'text', value: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', options: {}
+    },
+    {
+        type: 'textColumns', value: [
+            '1 text',
+            '2 text',
+            '3 text'
+        ],
+        options: {}
+    },
+]

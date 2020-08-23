4801b1798712c56c0ad1b8505418a7551e6f211d
Alexander <50077233+Sekir0@users.noreply.github.com>
2020-08-23 16:53:35 +0300

    refactored code using classes

--- src/model.js
+++ src/model.js
@@ -1,23 +1,26 @@
+import image from './img/image.jpg'
+import {TitleBlock, TextColumnBlock, ImageBlock, TextBlock} from './classes/blocks'
+
+const text = `Lorem ipsum dolor sit, amet consectetur adipisicing elit.`
+
+
 export const model = [
-    {
-        type: 'title', value: 'Test title', options: {
-            tag: 'h1',
-            styles: 'background: darkred; color: #fff; text-align: center;'
-        }
-    },
-    {
-        type: 'text', value: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', options: {
-            styles: 'background: darkblue; color: white;'
-        }
-    },
-    {
-        type: 'textColumns', value: [
-            '1 text',
-            '2 text',
-            '3 text'
-        ],
-        options: {
-            styles: 'padding 0.1rem;'
-        }
-    },
+    new TitleBlock('Test title', {
+        tag: 'h1',
+        styles: 'background: linear-gradient(to right, #ff0099, #493240); color: #fff; padding: 1.5rem; text-align: center;'
+    }),
+    new TextColumnBlock([
+        '1 text',
+        '2 text',
+        '3 text'
+        ],{
+        styles: 'background: linear-gradient(to right, blue, yellow); color: #fff; padding: 1.5rem; text-align: center;'
+    }),
+    new ImageBlock(image, {
+        alt: 'img',
+        imageStyles: 'width: 1100px; height: 550px;'
+    }),
+    new TextBlock(text, {
+        styles: 'background: linear-gradient(to right, #ff0099, #493240); padding: 1rem; font-weight: bolt;'
+    }),
 ]

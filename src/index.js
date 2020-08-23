0f6dd779c5b889e1d88c2490d5475ff8302cd14a
Alexander <50077233+Sekir0@users.noreply.github.com>
2020-08-23 17:54:21 +0300

    Added constructor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import {model} from './model'
+import { Site } from './classes/site'
+import { Sidebar } from './classes/sidebar'
 import './styles/main.css'
 
-const site = document.querySelector('#site')
 
-model.forEach(block => {
-    site.insertAdjacentHTML('beforeend', block.toHTML())
-})
+const site = new Site('#site')
+
+const updateCallback = newBlock => {
+    model.push(newBlock)
+    site.render(model)
+}
+
+new Sidebar('#panel', updateCallback)
+
+site.render(model)
 
 
--- src/model.js
+++ src/model.js
@@ -1,5 +1,6 @@
 import image from './img/image.jpg'
 import {TitleBlock, TextColumnBlock, ImageBlock, TextBlock} from './classes/blocks'
+import {css} from './utils'
 
 const text = `Lorem ipsum dolor sit, amet consectetur adipisicing elit.`
 
@@ -7,20 +8,40 @@ const text = `Lorem ipsum dolor sit, amet consectetur adipisicing elit.`
 export const model = [
     new TitleBlock('Test title', {
         tag: 'h1',
-        styles: 'background: linear-gradient(to right, #ff0099, #493240); color: #fff; padding: 1.5rem; text-align: center;'
+        styles: css({
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            padding: '1.5rem',
+            'text-align': 'center'
+        })
     }),
     new TextColumnBlock([
         '1 text',
         '2 text',
         '3 text'
         ],{
-        styles: 'background: linear-gradient(to right, blue, yellow); color: #fff; padding: 1.5rem; text-align: center;'
+        styles: css({
+            background: 'linear-gradient(to right, blue, yellow)',
+            color: '#fff',
+            padding: '1.5rem',
+            'text-align': 'center'
+        })
     }),
     new ImageBlock(image, {
+        styles: css({
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center'
+        }),
         alt: 'img',
-        imageStyles: 'width: 1100px; height: 550px;'
+        imageStyles: 'width: 1000px; height: 500px;'
     }),
     new TextBlock(text, {
-        styles: 'background: linear-gradient(to right, #ff0099, #493240); padding: 1rem; font-weight: bolt;'
+        styles: css({
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            padding: '1rem',
+            'font-weight': 'bolt'
+        })
     }),
 ]
